3878ef201fa5d280833aa550c9f47bf3fe1426c8
grigorev-web <83860144+grigorev-web@users.noreply.github.com>
2021-07-31 22:53:35 +0300

    all_icons

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,17 @@
-import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaBars,
+  FaChartPie,
+  FaCog,
+  FaHeadset,
+  FaSignOutAlt,
+  FaTimes,
+  FaUsers,
+  FaUserShield
+} from "react-icons/fa";
 import { reducer } from "../store/reducer";
 import OutsideAlerter from "./OutsideAlerter";
-//<div className={`side-menu ${props.active ? "active" : ""}`}>
+import msContactLogo from "../static/img/favicon.png";
+
 export default function SideMenu(props) {
   const MenuBars = () => {
     if (!!props.active) {
@@ -14,12 +24,37 @@ export default function SideMenu(props) {
   return (
     <OutsideAlerter clickOutsideMenu={props.clickOutsideMenu}>
       <div className={`side-menu ${props.active ? "active" : ""}`}>
-        <p>Side Menu</p>
         <div
           className="menu-toggle-btn-wrapper"
           onClick={props.onToggleMenuBtn}
         >
           <MenuBars />
+        </div>
+        <div className="menu-header">
+          <div className="menu-logo">
+            <img width="18px" src={msContactLogo} alt="HR CRM" />
+            HR CRM
+          </div>
+        </div>
+        <div className="menu-body">
+          <p>
+            <FaHeadset className="menu-icon" /> <span>Оператор</span>
+          </p>
+          <p>
+            <FaUsers className="menu-icon" /> <span>Рекрутер</span>
+          </p>
+          <p>
+            <FaUserShield className="menu-icon" /> <span>Администратор</span>
+          </p>
+          <p>
+            <FaChartPie className="menu-icon" /> <span>Статистка</span>
+          </p>
+          <p>
+            <FaCog className="menu-icon" /> <span>Настройки</span>
+          </p>
+          <p>
+            <FaSignOutAlt className="menu-icon" /> <span>Выйти</span>
+          </p>
         </div>
       </div>
     </OutsideAlerter>
